05aced2a16dd7293cd335f9a734b6b33335e7c6f
Abinaya0530 <abinaya.m2023a@vitstudent.ac.in>
2026-04-13 10:55:14 +0530

    Update AppTest.java

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -5,14 +5,14 @@ import org.junit.jupiter.api.Test;
 
 class VoterTest {
     @Test
-    void testEligible() {
-        Voter v = new Voter("Amit", 25, "Indian", true);
+    void testEligibleVoter() {
+        Voter v = new Voter("John", 25, "Indian", true);
         assertEquals("Eligible to Vote", v.checkEligibility());
     }
 
     @Test
     void testUnderage() {
-        Voter v = new Voter("Rahul", 17, "Indian", true);
-        assertEquals("Ineligible: Underage", v.checkEligibility());
+        Voter v = new Voter("Jane", 17, "Indian", true);
+        assertEquals("Ineligible: Underage (Must be 18+)", v.checkEligibility());
     }
 }
