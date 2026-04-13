146f3dc7ffdfc1d76141dffab685db417f3ffbc4
Abinaya0530 <abinaya.m2023a@vitstudent.ac.in>
2026-04-13 10:45:54 +0530

    Update AppTest.java

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,38 +1,18 @@
+package com.voting;
+
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
-class AppTest {
- App app = new App();
- @Test
- void testCalculateAverage() {
- int[] marks = {80, 90, 100};
- assertEquals(90.0, app.calculateAverage(marks));
- }
- @Test
- void testGradeA() {
- assertEquals("A", app.calculateGrade(92));
- }
- @Test
- void testGradeB() {
- assertEquals("B", app.calculateGrade(80));
- }
- @Test
- void testGradeC() {
- assertEquals("C", app.calculateGrade(65));
- }
- @Test
- void testGradeD() {
- assertEquals("D", app.calculateGrade(55));
- }
- @Test
- void testGradeF() {
- assertEquals("F", app.calculateGrade(40));
- }
- @Test
- void testEmptyMarks() {
- assertThrows(IllegalArgumentException.class, () -> {
- app.calculateAverage(new int[]{});
- });
- }
 
+class VoterTest {
+    @Test
+    void testEligible() {
+        Voter v = new Voter("Amit", 25, "Indian", true);
+        assertEquals("Eligible to Vote", v.checkEligibility());
+    }
+
+    @Test
+    void testUnderage() {
+        Voter v = new Voter("Rahul", 17, "Indian", true);
+        assertEquals("Ineligible: Underage", v.checkEligibility());
+    }
 }
-
